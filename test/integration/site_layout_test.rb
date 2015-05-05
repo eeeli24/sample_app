78539c83b1bd4ff2f6ea17e72041513e1bb191ef
Ilia Khudiashov <ilia.khudiashov@gmail.com>
2015-05-05 20:36:26 +0300

    finish updating-users exercises

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -1,19 +1,31 @@
 require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
-    
-    test 'layout links' do 
-      get root_path
-      assert_template 'static_pages/home'
-      assert_select "a[href=?]", root_path, count: 2
-      assert_select "a[href=?]", help_path
-      assert_select "a[href=?]", about_path
-      assert_select "a[href=?]", contact_path
-    end
 
-    test 'signup title' do
-      get signup_path
-      assert_response :success
-      assert_select "title", full_title("Sign up")
-    end
+  def setup
+    @admin = users(:michael)
+  end
+
+  test 'layout links' do
+    get root_path
+    assert_template 'static_pages/home'
+    assert_select "a[href=?]", root_path, count: 2
+    assert_select "a[href=?]", help_path
+    assert_select "a[href=?]", about_path
+    assert_select "a[href=?]", contact_path
+    assert_select "a[href=?]", login_path
+    assert_select "a[href=?]", users_path, count: 0
+    assert_select "a[href=?]", logout_path, count: 0
+    log_in_as(@admin) 
+    get root_path
+    assert_select "a[href=?]", login_path, count: 0
+    assert_select "a[href=?]", users_path
+    assert_select "a[href=?]", logout_path
+  end
+
+  test 'signup title' do
+    get signup_path
+    assert_response :success
+    assert_select "title", full_title("Sign up")
+  end
 end
